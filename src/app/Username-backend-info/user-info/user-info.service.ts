d9fafe4fd65dd58c5e6a013078948d6beb370bb2
nathanmullins11 <ndm00002@mix.wvu.edu>
2023-03-05 11:03:15 -0500

    Created database for Username information

---
+++ src/app/Username-backend-info/user-info/user-info.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { UserInfo } from "./user-info-model";
+
+@Injectable({providedIn: 'root'})
+export class UserInfoService {
+  private baseUrl: string = 'https://got-next-app-default-rtdb.firebaseio.com/';
+  private myInfoEndpoint = 'User-info.json';
+  constructor(private http:HttpClient) {
+    
+  }
+
+  getUserInfo() {
+    console.log(this.baseUrl + this.myInfoEndpoint);
+    return this.http.get<UserInfo>(this.baseUrl + this.myInfoEndpoint);
+  }
+
+  modifyUserInfo(data:UserInfo) {
+   
+    return this.http.put(this.baseUrl + this.myInfoEndpoint, data);
+}
+
+}
