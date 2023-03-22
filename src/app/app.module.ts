038e5fe0fd9173b1f7c97f513515b764e7b80515
Ian Jackson <iansjackson8@gmail.com>
2023-03-21 21:35:56 -0400

    create scan QR page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,8 @@ import { PartyInfoComponent } from './backend/Partyname-backend-info/party-info/
 import { QrPageComponent } from './qr-page/qr-page.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
-import { PartyEntryPageComponent } from './party-entry-page/party-entry-page.component';
+import { PartyEntryPageComponent } from './party-entry/party-entry-page/party-entry-page.component';
+import { ScanQrComponent } from './party-entry/scan-qr/scan-qr.component';
 
 @NgModule({
   declarations: [
@@ -37,7 +38,8 @@ import { PartyEntryPageComponent } from './party-entry-page/party-entry-page.com
     QrPageComponent,
     NavbarComponent,
     FooterComponent,
-    PartyEntryPageComponent
+    PartyEntryPageComponent,
+    ScanQrComponent
   ],
   imports: [
     BrowserModule,
