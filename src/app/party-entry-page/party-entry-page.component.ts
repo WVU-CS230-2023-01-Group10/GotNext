8325a143d8e4a246970df104f7a0f451209a78d3
Ian Jackson <iansjackson8@gmail.com>
2023-03-21 20:34:38 -0400

    populate party entry page

--- src/app/party-entry-page/party-entry-page.component.ts
+++ src/app/party-entry-page/party-entry-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-party-entry-page',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./party-entry-page.component.css']
 })
 export class PartyEntryPageComponent {
+  partyCode: string = ''
 
+  constructor(private router: Router) {
+
+  }
+
+  getPartyCode() {
+    // set party code to value in input field 
+    this.partyCode = (<HTMLInputElement>document.getElementById("partyCode")).value
+    
+    // TODO: Check if party code is valid
+    /* validatePartyCode(this.partyCode) */
+
+    // if party code valid, route to user login page
+    this.router.navigate(['/userlogin'])
+
+    // if not, alert user
+  }
 }
+
