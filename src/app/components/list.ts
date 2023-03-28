6f4fa60ac41c6b9646932d33060356929de3f650
srm00022 <123519427+srm00022@users.noreply.github.com>
2023-03-27 22:41:57 -0400

    forgot to comment stuff, added one more function

--- src/app/components/list.ts
+++ src/app/components/list.ts
@@ -17,6 +17,8 @@ export class linkedList<T> {
     }
 
 
+
+
 /**
  * adds an element to the end of the list
  * 
@@ -43,6 +45,11 @@ export class linkedList<T> {
         }
     }
 
+
+    /**basic is empty function
+     * 
+     * @returns true if the list is empty, false otherwise
+     */
     public isEmpty() : boolean{
 
         if (this.size==0){
@@ -54,6 +61,11 @@ export class linkedList<T> {
         }
     }
 
+    /**searches for an removes a specific element by its data
+     * 
+     * @param data specific data element to be removed
+     * @returns  true if removal was sucessful, false otherwise
+     */
     public remove(data: T) : boolean{
         if (this.isEmpty()){
             return false;
@@ -85,17 +97,55 @@ export class linkedList<T> {
 
     }
 
+    /** this function was in our component documentation as get, supposed to be here to move people from an unpartnered list to a partnered one or something like that.
+     * I changed the name to partnerUp for now
+     * @param data1 first data to move
+     * @param data2 second data to move
+     * @param addTo list for data to be moved to
+     * @returns true if all operations are successful
+     */
     public partnerUp(data1: T, data2: T, addTo: linkedList<T>) : boolean{
-        if (this.remove(data1) && this.remove(data2)){
-            if(addTo.add(data1) && addTo.add(data2)){
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
-        else{
+        if (this.isEmpty()){
             return false;
         }
+
+      if ((this.find(data1)!=-1) && (this.find(data2)!=-1) && (addTo.find(data2)==-1) && (addTo.find(data2)==-1)){
+        this.remove(data1);
+        this.remove(data2);
+        addTo.add(data1);
+        addTo.add(data2);
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+
+
+
+    /**used to find an element's index if it has one, otherwise
+     * 
+     * @param data data to find an index of
+     * @returns the index of where the element was found, -1 if not found. I don't use this in add or remove because it would end up doing a double traversal, and
+     * I also wrote this function after I wrote add and remove...
+     */
+    public find(data: T): number{
+
+        if (this.isEmpty()){
+            return -1;
+        }
+        var count: number = 0;
+        
+
+        for( var i: NodeI<T> | null = this.head; i != null; i = i?.getNextNode()){
+            if (i.getData() == data){
+                return count;
+            }
+            count++;
+        }
+        return -1;
     }
 }
+
+
+
