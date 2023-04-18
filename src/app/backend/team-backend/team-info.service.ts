6a4a77edec2b26e50af040cbf7d871f71d69b7dc
Ian Jackson <iansjackson8@gmail.com>
2023-04-18 15:31:03 -0400

    work to update player count

--- src/app/backend/team-backend/team-info.service.ts
+++ src/app/backend/team-backend/team-info.service.ts
@@ -8,7 +8,7 @@ import { FloatingUserInfoService } from "../floatinguser-backend/floatinguser-in
 import { QueuePageService } from "../fetching-data/queue-data/game-page.service";
 import { CodeInfoService } from "../partycode-backend/code-info.service";
 import { Router } from "@angular/router";
-import { Subject } from "rxjs";
+import { Subject, map } from "rxjs";
 import { GameInfoService } from "../game-backend/game-info.service";
 
 @Injectable({providedIn: 'root'})
@@ -27,98 +27,130 @@ export class TeamInfoService {
   timestamp: number = 0;
  // User2: string='Not set yet';
 
-/**
- * adds user to chosen party under FloatingUser node within a specific party
- */
-addTeam(partyCodeInfo: CodeInfo, team: TeamInfo, gameName: string) {
-  const ref = this.db.list<TeamInfo>(`Party/${partyCodeInfo.Partycode}/Games/${gameName}/Teams`).query.ref;
-  ref.child(team.User1).set(team);
-  // update player counter
-  this.playerCount = this.playerCount++;
-}
+  /**
+   * adds user to chosen party under FloatingUser node within a specific party
+   */
+  addTeam(partyCodeInfo: CodeInfo, team: TeamInfo, gameName: string) {
+    const ref = this.db.list<TeamInfo>(`Party/${partyCodeInfo.Partycode}/Games/${gameName}/Teams`).query.ref;
+    ref.child(team.User1).set(team);
+    // update player counter
+    this.updatePlayerCount(partyCodeInfo, gameName, "inc");
+  }
 
-/**
- * removes user from team node
- * @param partyCodeInfo 
- * @param team 
- * @param gameName 
- */
-deleteTeam(partyCodeInfo: CodeInfo, team: string, gameName: string) {
-  const ref = this.db.list<TeamInfo>(`Party/${partyCodeInfo.Partycode}/Games/${gameName}/Teams`).query.ref;
-  ref.child(team).remove();
-  // update player counter
-  this.playerCount = this.playerCount--;
-}
+  /**
+   * removes user from team node
+   * @param partyCodeInfo 
+   * @param team 
+   * @param gameName 
+   */
+  deleteTeam(partyCodeInfo: CodeInfo, team: string, gameName: string) {
+    const ref = this.db.list<TeamInfo>(`Party/${partyCodeInfo.Partycode}/Games/${gameName}/Teams`).query.ref;
+    ref.child(team).remove();
+    // update player counter
+    this.playerCount = this.playerCount--;
+  }
 
-/**
- * allows user to exit Queue and return to game list page, removes user from game & team nodes
- * @param partyCodeInfo code of current party
- * @param team username of player
- * @param gameName game selected
- */
-exitQueue(partyCodeInfo: string, team: string, gameName: string) {
-  const ref = this.db.list<TeamInfo>(`Party/${partyCodeInfo}/Games/${gameName}/Teams`).query.ref;
-  ref.child(team).remove();
-  // update player count
-  this.playerCount = this.playerCount--;
-  //ref.child(floatingUserInfo.User2).remove();
-}
+  /**
+   * allows user to exit Queue and return to game list page, removes user from game & team nodes
+   * @param partyCodeInfo code of current party
+   * @param team username of player
+   * @param gameName game selected
+   */
+  exitQueue(partyCodeInfo: string, team: string, gameName: string) {
+    const ref = this.db.list<TeamInfo>(`Party/${partyCodeInfo}/Games/${gameName}/Teams`).query.ref;
+    ref.child(team).remove();
+    // update player count
+    this.playerCount = this.playerCount--;
+    //ref.child(floatingUserInfo.User2).remove();
+  }
 
-/**
- * adds the two teams who are currently playing to a separate node
- * @param partyCodeInfo 
- * @param team 
- * @param gameName 
- */
-addUpNow(partyCodeInfo: CodeInfo, team: string, gameName: string) {
-  const ref = this.db.list<TeamInfo>(`Party/${partyCodeInfo.Partycode}/Games/${gameName}/UpNow`).query.ref;
-  ref.child(team).set(team);
-}
+  /**
+   * adds the two teams who are currently playing to a separate node
+   * @param partyCodeInfo 
+   * @param team 
+   * @param gameName 
+   */
+  addUpNow(partyCodeInfo: CodeInfo, team: string, gameName: string) {
+    const ref = this.db.list<TeamInfo>(`Party/${partyCodeInfo.Partycode}/Games/${gameName}/UpNow`).query.ref;
+    ref.child(team).set(team);
+  }
 
-/**
- * checks if current user on site is up to play in the UpNow node
- * @param partyCodeInfo 
- * @param team 
- * @param gameName 
- * @returns true if user is in UpNow node
- */
-async checkIfInUpNow(partyCodeInfo: CodeInfo, team: string, gameName: string): Promise<boolean> {
-  const ref = this.db.list<TeamInfo>(`Party/${partyCodeInfo.Partycode}/Games/${gameName}/UpNow`).query.ref;
-  const snapshot = await ref.child(team).once('value');
-  return snapshot.exists();
-}
+  /**
+   * checks if current user on site is up to play in the UpNow node
+   * @param partyCodeInfo 
+   * @param team 
+   * @param gameName 
+   * @returns true if user is in UpNow node
+   */
+  async checkIfInUpNow(partyCodeInfo: CodeInfo, team: string, gameName: string): Promise<boolean> {
+    const ref = this.db.list<TeamInfo>(`Party/${partyCodeInfo.Partycode}/Games/${gameName}/UpNow`).query.ref;
+    const snapshot = await ref.child(team).once('value');
+    return snapshot.exists();
+  }
 
-/**
- * adds teams who check in into CurrentlyPlaying node
- * @param partyCodeInfo 
- * @param team 
- * @param gameName 
- */
-addCurrentlyPlaying(partyCodeInfo: CodeInfo, team: string, gameName: string) {
-  const ref = this.db.list<TeamInfo>(`Party/${partyCodeInfo.Partycode}/Games/${gameName}/CurrentlyPlaying`).query.ref;
-  ref.child(team).set(team);
-}
+  /**
+   * adds teams who check in into CurrentlyPlaying node
+   * @param partyCodeInfo 
+   * @param team 
+   * @param gameName 
+   */
+  addCurrentlyPlaying(partyCodeInfo: CodeInfo, team: string, gameName: string) {
+    const ref = this.db.list<TeamInfo>(`Party/${partyCodeInfo.Partycode}/Games/${gameName}/CurrentlyPlaying`).query.ref;
+    ref.child(team).set(team);
+  }
 
-/**
- * Remove user from UpNow node
- * @param partyCodeInfo 
- * @param floatingUserInfo 
- */
-deleteUpNow(partyCodeInfo: CodeInfo, team: string, gameName: string) {
-  const ref = this.db.list<TeamInfo>(`Party/${partyCodeInfo.Partycode}/Games/${gameName}/UpNow`).query.ref;
-  ref.child(team).remove();
-}
+  /**
+   * Remove user from UpNow node
+   * @param partyCodeInfo 
+   * @param floatingUserInfo 
+   */
+  deleteUpNow(partyCodeInfo: CodeInfo, team: string, gameName: string) {
+    const ref = this.db.list<TeamInfo>(`Party/${partyCodeInfo.Partycode}/Games/${gameName}/UpNow`).query.ref;
+    ref.child(team).remove();
+  }
 
-/**
- * remove user from CurrentlyPlaying node
- * @param partyCodeInfo 
- * @param team 
- * @param gameName 
- */
-deleteCurrentlyPlaying(partyCodeInfo: CodeInfo, team: string, gameName: string) {
-  const ref = this.db.list<TeamInfo>(`Party/${partyCodeInfo.Partycode}/Games/${gameName}/CurrentlyPlaying`).query.ref;
-  ref.child(team).remove();
-}
+  /**
+   * remove user from CurrentlyPlaying node
+   * @param partyCodeInfo 
+   * @param team 
+   * @param gameName 
+   */
+  deleteCurrentlyPlaying(partyCodeInfo: CodeInfo, team: string, gameName: string) {
+    const ref = this.db.list<TeamInfo>(`Party/${partyCodeInfo.Partycode}/Games/${gameName}/CurrentlyPlaying`).query.ref;
+    ref.child(team).remove();
+  }
 
+  // TODO: Issue with updating player count
+  updatePlayerCount(partyCodeInfo: CodeInfo, gameName: string, mode: string) {
+    var count: number = -1;
 
+    // determine increment or decrement
+    if (mode === "inc") {
+      console.log("inc")
+      this.db.list<number>(`Party/${partyCodeInfo.Partycode}/Games/${gameName}/NumPlayers`).valueChanges()
+      .pipe(
+        map(values => values[0]) // Assuming NumPlayers is a single-value array, extract the first value
+      )
+      .subscribe(value => {
+        if (typeof value === 'number') {
+          count = value + 1;
+          console.log(count, value)
+          const ref = this.db.list<TeamInfo>(`Party/${partyCodeInfo.Partycode}/Games/${gameName}/NumPlayers`).query.ref;
+          ref.set(count);
+        }
+      });
+    } else if (mode === "dec") {
+      this.db.list<number>(`Party/${partyCodeInfo.Partycode}/Games/${gameName}/NumPlayers`).valueChanges()
+      .pipe(
+        map(values => values[0]) // Assuming NumPlayers is a single-value array, extract the first value
+      )
+      .subscribe(value => {
+        if (typeof value === 'number') {
+          count = value - 1;
+          const ref = this.db.list<TeamInfo>(`Party/${partyCodeInfo.Partycode}/Games/${gameName}/NumPlayers`).query.ref;
+          ref.set(count);
+        }
+      });
+    }
+  }
 }
